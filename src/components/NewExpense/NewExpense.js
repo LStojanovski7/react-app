a59560fc6ed2db7c1f4fd0210da5fcaf08785af8
LazarStojanovski <lazar.stojanovski@sopro.io>
2022-02-01 16:56:13 +0100

    Fixes and changes, added new components

---
+++ src/components/NewExpense/NewExpense.js
@@ -0,0 +1,27 @@
+import './NewExpense.css';
+
+import ExpenseForm from '../ExpenseForm/ExpenseForm';
+import expenses from '../../data';
+
+const NewExpense = (props) => {
+
+    const saveExpenseDataHandler = (enteredExpenseData) => {
+
+        const expenseData = {
+            ...enteredExpenseData,
+            id: Math.random().toString(),
+        };
+
+        props.onAddExpense(expenseData);
+    };
+
+
+
+    return (
+        <div className='new-expense'>
+            <ExpenseForm onSubmitExpenseData={saveExpenseDataHandler} />
+        </div>
+    );
+};
+
+export default NewExpense;
